71c59aea7121bbc3b6d1382bc0affc27b5265c43
Anirudh Batra <hello@anirudhbatraofficial.com>
2025-03-10 20:55:09 +0530

    Added Tools Section and Updated Navbar

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -3,13 +3,13 @@ import { Link } from 'react-router-dom';
 export default function Navbar() {
   return (
     <nav className="p-4 bg-gray-800 text-white">
-      <ul className="flex justify-center space-x-6">  {/* Centered and spaced evenly */}
-        <li><Link to="/" className="hover:text-yellow-400">Home</Link></li>
-        <li><Link to="/about" className="hover:text-yellow-400">About</Link></li>
-        <li><Link to="/projects" className="hover:text-yellow-400">Projects</Link></li>
-        <li><Link to="/blog" className="hover:text-yellow-400">Blog</Link></li>
-        <li><Link to="/tools" className="hover:text-yellow-400">Tools</Link></li> {/* Added Tools */}
-        <li><Link to="/contact" className="hover:text-yellow-400">Contact</Link></li>
+      <ul className="flex justify-center space-x-8">  {/* Ensures horizontal alignment */}
+        <li><Link to="/" className="hover:text-yellow-400 px-4 py-2">Home</Link></li>
+        <li><Link to="/about" className="hover:text-yellow-400 px-4 py-2">About</Link></li>
+        <li><Link to="/projects" className="hover:text-yellow-400 px-4 py-2">Projects</Link></li>
+        <li><Link to="/tools" className="hover:text-yellow-400 px-4 py-2">Tools</Link></li>
+        <li><Link to="/blog" className="hover:text-yellow-400 px-4 py-2">Blog</Link></li>
+        <li><Link to="/contact" className="hover:text-yellow-400 px-4 py-2">Contact</Link></li>
       </ul>
     </nav>
   );
