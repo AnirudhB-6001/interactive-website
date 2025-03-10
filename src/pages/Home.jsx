40a9cbdc68a26392cf8e5160aa717831631c5519
Anirudh Batra <hello@anirudhbatraofficial.com>
2025-03-10 19:07:05 +0530

    Changed LinkedIn Link

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -62,7 +62,7 @@ const Home = () => {
         <p>Let's connect and build something amazing.</p>
         <div className="social-links">
           <a href="https://github.com/AnirudhB-6001" target="_blank">GitHub</a>
-          <a href="https://www.linkedin.com/in/anirudh-batra" target="_blank">LinkedIn</a>
+          <a href="https://www.linkedin.com/in/anirudh-batra-bd" target="_blank">LinkedIn</a>
         </div>
       </div>
     </div>
