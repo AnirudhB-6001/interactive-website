b9834e4f007bef12806ebb8e4a03816f3dafea75
Anirudh Batra <hello@anirudhbatraofficial.com>
2025-03-10 21:27:20 +0530

    Updated NavBar

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,15 +1,16 @@
 import { Link } from 'react-router-dom';
+import "../styles/navbar.css";  // Ensure CSS file is imported
 
 export default function Navbar() {
   return (
-    <nav className="p-4 bg-gray-800 text-white">
-      <ul className="flex justify-center space-x-8">  {/* Ensures horizontal alignment */}
-        <li><Link to="/" className="hover:text-yellow-400 px-4 py-2">Home</Link></li>
-        <li><Link to="/about" className="hover:text-yellow-400 px-4 py-2">About</Link></li>
-        <li><Link to="/projects" className="hover:text-yellow-400 px-4 py-2">Projects</Link></li>
-        <li><Link to="/tools" className="hover:text-yellow-400 px-4 py-2">Tools</Link></li>
-        <li><Link to="/blog" className="hover:text-yellow-400 px-4 py-2">Blog</Link></li>
-        <li><Link to="/contact" className="hover:text-yellow-400 px-4 py-2">Contact</Link></li>
+    <nav className="navbar">
+      <ul>
+        <li><Link to="/">Home</Link></li>
+        <li><Link to="/about">About</Link></li>
+        <li><Link to="/projects">Projects</Link></li>
+        <li><Link to="/tools">Tools</Link></li>
+        <li><Link to="/blog">Blog</Link></li>
+        <li><Link to="/contact">Contact</Link></li>
       </ul>
     </nav>
   );
